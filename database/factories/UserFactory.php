c560c27e381f161046558214fe255a599596a1fa
HannanZikan <hannanczikan@gmail.com>
2025-03-11 12:06:00 -0300

    feat: implemented factories to create to-do list for the created user by UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,12 +2,14 @@
 
 namespace Database\Factories;
 
+use App\Models\TodoList;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends Factory<User>
  */
 class UserFactory extends Factory
 {
@@ -40,5 +42,22 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    /**
+     * Create a user with a specific number of TodoLists.
+     *
+     * @param int $count
+     * @return Factory
+     */
+    public function withTodoLists(int $count = 10): Factory
+    {
+        return $this->afterCreating(function (User $user) use ($count) {
+            TodoList::factory()
+                ->count($count)
+                ->create([
+                    'user_id' => $user->id
+                ]);
+        });
     }
 }
